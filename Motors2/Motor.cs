bf9958a44126c00d0a80ec8215e38fddd6ac6fce
Petrik Diak <diak@petrik.hu>
2025-03-14 10:22:12 +0100

    motor construkor

---
+++ Motors2/Motor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Motors2
+{
+	internal class Motor
+	{
+		string brand;
+		string name;
+		int releaseYear;
+		double performance;
+		double priceInEur;
+
+		public Motor(string brand, string name, int releaseYear, double performance, double priceInEur)
+		{
+			this.brand = brand;
+			this.name = name;
+			this.releaseYear = releaseYear;
+			this.performance = performance;
+			this.priceInEur = priceInEur;
+		}
+	}
+}
